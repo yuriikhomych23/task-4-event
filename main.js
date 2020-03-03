231ab2a9f8d27cd5583609dd035120a561a2d8b8
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-03-03 21:13:40 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,62 +1,41 @@
 let getId = x => document.getElementById(x);
 let getSel = x => document.querySelector(x);
 
-function changeSize() {
-    getId('red').style.color = 'red';
-    getId('yellow').style.color = 'yellow';
-    getId('green').style.color = 'green';
-    getId('blue').style.color = 'blue';
-    getId('orange').style.color = 'orange';
-    getId('grey').style.color = 'grey';
-    console.log(this);
+// function changeSize() {
+//     getId('red').style.color = 'red';
+//     getId('yellow').style.color = 'yellow';
+//     getId('green').style.color = 'green';
+//     getId('blue').style.color = 'blue';
+//     getId('orange').style.color = 'orange';
+//     getId('grey').style.color = 'grey';
+//     console.log(this);
+// }
+
+// getSel('.click').onclick = changeSize;
+// стрався зробити через одну функцію але десь помиляюсь і неможу зрозуміти де, або взагалі неправильно роблю 
+
+
+
+getId('red').onclick = function() {
+    getId('red').style.color = 'red'
 }
 
-getSel('.click').onclick = changeSize;
+getId('green').onclick = function() {
+    getId('green').style.color = 'green'
+}
 
+getId('yellow').onclick = function() {
+    getId('yellow').style.color = 'yellow'
+}
 
+getId('blue').onclick = function() {
+    getId('blue').style.color = 'blue'
+}
 
+getId('orange').onclick = function() {
+    getId('orange').style.color = 'orange'
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// getId('red').onclick = function() {
-//     getId('red').style.color = 'red'
-// }
-
-// getId('green').onclick = function() {
-//     getId('green').style.color = 'green'
-// }
-
-// getId('yellow').onclick = function() {
-//     getId('yellow').style.color = 'yellow'
-// }
-
-// getId('blue').onclick = function() {
-//     getId('blue').style.color = 'blue'
-// }
-
-// getId('orange').onclick = function() {
-//     getId('orange').style.color = 'orange'
-// }
-
-// getId('grey').onclick = function() {
-//     getId('grey').style.color = 'grey'
-// }
+getId('grey').onclick = function() {
+    getId('grey').style.color = 'grey'
+}
